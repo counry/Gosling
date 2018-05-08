451f388e925cb174f3089003e7b37ac5ab5d84b2
zhounianhua <zhounianhua@meituan.com>
2018-05-08 11:58:55 +0800

    add graph

--- src/main/java/ListMap.java
+++ src/main/java/ListMap.java
@@ -1,16 +1,70 @@
 
 import java.util.*;
 
-public class FusionSchemaTest {
+public class ListMap {
+
+	public static String sall;
+
+	public ListMap() {
+		sall = "";
+	}
 
 	public void print(String s) {
 		System.out.println("this in print function: " + s);	
 	}
 
+	public static void add(String s) {sall += s;}
+
+	public void get_list(List<String> l) {l.add("bbbbbccccc");}
+
+	enum TagValueType {
+		Boolean,
+		Text,
+		Enumeration,
+		Int,
+		Real,
+		Unknown
+	}
+
+	enum CatType {
+		Empty(0),
+		Poi(1),
+		Building(2),
+		Transportation(4),
+		Use(8),
+		Name(16),
+		PseudoName(32),
+		HgisPoi(64),
+		All(1|2|4|8|16|64);
+
+		private final int value;
+
+		private CatType(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+	}
+
+	public void testcattype(CatType t) {
+		System.out.println(t);
+	}
+
+	public TagValueType tvt =  TagValueType.Enumeration;
+
 	public static void main(String[] args) {
+		ListMap t = new ListMap();
+		System.out.println(t.sall.isEmpty());
 		System.out.println("hello world");
+
+
 		String s1 = "=abc";
 		String s2 = "abc=abc";
+
+		System.out.println(s1.indexOf('='));
+		System.out.println(s2.indexOf('='));
 
 		String info1 [] =  s1.split("=");
 		String info2 [] =  s2.split("=");
@@ -29,6 +83,71 @@ public class FusionSchemaTest {
 		for (String s : l1) {
 			System.out.println(s);
 		}
-		
+
+		System.out.println("test map:");
+		Map<String, String> hm = new HashMap<>();
+		hm.put("a", "aa");
+		hm.put("b", "");
+		hm.put("c", "cc");
+		hm.forEach((k, v) -> System.out.println(k+"->"+v));
+		hm.compute("a", (k, v) -> v + "new");
+		hm.merge("b", "c", (k, v) -> v + "new");
+		hm.forEach((k, v) -> System.out.println(k+"->"+v));
+		String v = hm.get("d");
+		//System.out.println(v.isEmpty());
+
+		List<String> l2 = new ArrayList<>();
+		l2.add("ccc");
+		l2.forEach(n -> System.out.println(n));
+		t.get_list(l2);
+		l2.forEach(n -> System.out.println(n));
+
+		//compare
+		List<String> c1 = new ArrayList<>();
+		List<String> c2 = new ArrayList<>();
+		c1.add("caaa");
+		c1.add("cbbb");
+		c1.add("cccc");
+		c2.add("cbbb");
+		c2.add("cccc");
+		c2.add("caaa");
+		c1.forEach(n -> System.out.println(n));
+		c2.forEach(n -> System.out.println(n));
+		Collections.sort(c1);
+		Collections.sort(c2);
+		System.out.println(c1.equals(c2));
+
+		String s5 = "aaa;ttt;;ccc;dddd;;eeee;";
+		String[] l5 = s5.split(";");
+		for (String s : l5) {
+			System.out.println(s);
+		}
+
+		StringBuffer sb1 = new StringBuffer();
+		sb1.append("a");
+		sb1.append("c");
+		sb1.append("\n");
+		sb1.append("d");
+		System.out.println(sb1.toString());
+
+		List<String> c6 = new ArrayList<>();
+		c6.add("ggg");
+		String s6 = "aaa;ttt;;ccc;dddd;;eeee;";
+		String[] l6 = s6.split(";");
+		Collections.addAll(c6, l6);
+		System.out.println(c6.toString());
+
+		String l7 = "abc=";
+		int equalsPos = l7.indexOf("=");
+		System.out.println(l7.substring(0, equalsPos));
+		System.out.println(l7.substring(equalsPos+1));
+
+		ListMap lm7 = new ListMap();
+		System.out.println(lm7.tvt.toString());
+
+		CatType ct1 = CatType.HgisPoi;
+		lm7.testcattype(ct1);
+
+
 	}
 }
---
+++ src/main/java/SymbolDigraph.java
@@ -0,0 +1,234 @@
+/******************************************************************************
+ *  Compilation:  javac SymbolDigraph.java
+ *  Execution:    java SymbolDigraph
+ *  Dependencies: ST.java Digraph.java In.java
+ *  Data files:   https://algs4.cs.princeton.edu/42digraph/routes.txt
+ *  
+ *  %  java SymbolDigraph routes.txt " "
+ *  JFK
+ *     MCO
+ *     ATL
+ *     ORD
+ *  ATL
+ *     HOU
+ *     MCO
+ *  LAX
+ *
+ ******************************************************************************/
+
+
+/**
+ *  The {@code SymbolDigraph} class represents a digraph, where the
+ *  vertex names are arbitrary strings.
+ *  By providing mappings between string vertex names and integers,
+ *  it serves as a wrapper around the
+ *  {@link Digraph} data type, which assumes the vertex names are integers
+ *  between 0 and <em>V</em> - 1.
+ *  It also supports initializing a symbol digraph from a file.
+ *  <p>
+ *  This implementation uses an {@link ST} to map from strings to integers,
+ *  an array to map from integers to strings, and a {@link Digraph} to store
+ *  the underlying graph.
+ *  The <em>indexOf</em> and <em>contains</em> operations take time 
+ *  proportional to log <em>V</em>, where <em>V</em> is the number of vertices.
+ *  The <em>nameOf</em> operation takes constant time.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+
+import java.util.HashMap;
+
+public class SymbolDigraph {
+    private HashMap<String, Integer> st;  // string -> index
+    private String[] keys;           // index  -> string
+    private EdgeWeightedDigraph graph;           // the underlying digraph
+
+    /**  
+     * Initializes a digraph from a file using the specified delimiter.
+     * Each line in the file contains
+     * the name of a vertex, followed by a list of the names
+     * of the vertices adjacent to that vertex, separated by the delimiter.
+     * @param filename the name of the file
+     * @param delimiter the delimiter between fields
+     */
+    public SymbolDigraph(String filename, String delimiter) {
+        st = new HashMap<String, Integer>();
+
+        // First pass builds the index by reading strings to associate
+        // distinct strings with an index
+        In in = new In(filename);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(delimiter);
+            if (a.length == 3) {
+                for (int i = 0; i < 2; i++) {
+                    if (!st.containsKey(a[i]))
+                        st.put(a[i], st.size());
+                }
+            }
+        }
+
+        // inverted index to get string keys in an array
+        keys = new String[st.size()];
+        for (String name : st.keySet()) {
+            keys[st.get(name)] = name;
+        }
+
+        // second pass builds the digraph by connecting first vertex on each
+        // line to all others
+        graph = new EdgeWeightedDigraph(st.size());
+        in = new In(filename);
+        while (in.hasNextLine()) {
+            String[] a = in.readLine().split(delimiter);
+            if (a.length == 3) {
+                int v = st.get(a[0]);
+                int w = st.get(a[1]);
+                double weight = Double.parseDouble(a[2]);
+                graph.addEdge(new DirectedEdge(v, w, weight));
+            }
+        }
+    }
+
+    /**
+     * Does the digraph contain the vertex named {@code s}?
+     * @param s the name of a vertex
+     * @return {@code true} if {@code s} is the name of a vertex, and {@code false} otherwise
+     */
+    public boolean contains(String s) {
+        return st.containsKey(s);
+    }
+
+    /**
+     * Returns the integer associated with the vertex named {@code s}.
+     * @param s the name of a vertex
+     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
+     * @deprecated Replaced by {@link #indexOf(String)}.
+     */
+    @Deprecated
+    public int index(String s) {
+        return st.get(s);
+    }
+
+    /**
+     * Returns the integer associated with the vertex named {@code s}.
+     * @param s the name of a vertex
+     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
+     */
+    public int indexOf(String s) {
+        Object o = st.get(s);
+        return o == null ? -1 : ((Integer) o).intValue();
+    }
+
+    /**
+     * Returns the name of the vertex associated with the integer {@code v}.
+     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
+     * @return the name of the vertex associated with the integer {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @deprecated Replaced by {@link #nameOf(int)}.
+     */
+    @Deprecated
+    public String name(int v) {
+        validateVertex(v);
+        return keys[v];
+    }
+
+    /**
+     * Returns the name of the vertex associated with the integer {@code v}.
+     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
+     * @return the name of the vertex associated with the integer {@code v}
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public String nameOf(int v) {
+        validateVertex(v);
+        return keys[v];
+    }
+
+    /**
+     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
+     * not to mutate the digraph.
+     *
+     * @return the digraph associated with the symbol digraph
+     * @deprecated Replaced by {@link #digraph()}.
+     */
+    @Deprecated
+    public EdgeWeightedDigraph G() {
+        return graph;
+    }
+
+    /**
+     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
+     * not to mutate the digraph.
+     *
+     * @return the digraph associated with the symbol digraph
+     */
+    public EdgeWeightedDigraph digraph() {
+        return graph;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+         int V = graph.V();
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Unit tests the {@code SymbolDigraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String start = args[1];
+        SymbolDigraph sg = new SymbolDigraph(filename, " ");
+        EdgeWeightedDigraph graph = sg.digraph();
+        int s = sg.indexOf(start);
+        if (s == -1) {
+            System.out.println(start + "is not exist");
+            return;
+        }
+        // compute shortest paths
+        DijkstraSP sp = new DijkstraSP(graph, s);
+
+        // print shortest path
+        for (int t = 0; t < graph.V(); t++) {
+            if (sp.hasPathTo(t)) {
+                StdOut.printf("%s to %s (%.2f)  ", sg.nameOf(s), sg.nameOf(t), sp.distTo(t));
+                for (DirectedEdge e : sp.pathTo(t)) {
+                    StdOut.print(e + "   ");
+                }
+                StdOut.println();
+            }
+            else {
+                StdOut.printf("%s to %s         no path\n", sg.nameOf(s), sg.nameOf(t));
+            }
+        }
+    }
+}
+
+/******************************************************************************
+ *  Copyright 2002-2018, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
